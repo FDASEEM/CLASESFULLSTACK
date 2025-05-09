185e9e8094d09cdf202be4d0c91ddeed94f03456
Vina del Mar <vina@duoc.acad>
2025-05-08 20:23:05 -0400

    controlelr terminado

--- hospital_vm/src/main/java/com/hospital_vm/hospital_vm/Controller/PacienteController.java
+++ hospital_vm/src/main/java/com/hospital_vm/hospital_vm/Controller/PacienteController.java
@@ -1,15 +1,24 @@
 package com.hospital_vm.hospital_vm.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import com.hospital_vm.hospital_vm.Services.PacienteService;
+import com.hospital_vm.hospital_vm.Model.Paciente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/pacientes")
 public class PacienteController {
 
 
@@ -26,5 +35,65 @@ public ResponseEntity<List<Paciente>> listar() {
     return ResponseEntity.ok(pacientes);
 
 }
+ @PostMapping("path")
+ public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente) {
+ Paciente productoNuevo = pacienteService.save(paciente);
+ return ResponseEntity.status(HttpStatus.CREATED).body(productoNuevo);
 
+ }
+
+
+ @GetMapping("/{id}")
+ public ResponseEntity<Paciente> buscar(@PathVariable Integer id) {
+    try {
+        Paciente paciente = pacienteService.findId(id);
+        return ResponseEntity.ok(paciente); 
+    } catch (Exception e) {
+        return ResponseEntity.notFound().build();   }
+
+ }
+
+ @PutMapping("/{id}")
+ public ResponseEntity<Paciente> actualizar(@PathVariable Integer id, @RequestBody Paciente paciente) {
+    try {
+        Paciente pac = pacienteService.findId(id);
+        pac.setId(id);
+        pac.setRun(paciente.getRun());
+        pac.setNombre(paciente.getNombre());
+        pac.setApellido(paciente.getApellido());
+        pac.setFechaNacimiento(paciente.getFechaNacimiento());
+        pac.setCorreo(paciente.getCorreo());
+
+
+        pacienteService.save(pac);
+        return ResponseEntity.ok(paciente);
+
+    } catch ( Exception e) {
+
+        return ResponseEntity.notFound().build();
+    }
+ }
+
+ @DeleteMapping("/{id}")
+ public ResponseEntity<?> eliminar(@PathVariable Integer id) {
+    try {
+        pacienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    } catch (Exception e) {
+        return ResponseEntity.notFound().build();
+    }
+ }
 }
+
+     
+    
+
+ 
+
+
+
+
+
+//crear ramas en el git 
+//                      -- feature / implementacion
+//                      -- feature / dac
