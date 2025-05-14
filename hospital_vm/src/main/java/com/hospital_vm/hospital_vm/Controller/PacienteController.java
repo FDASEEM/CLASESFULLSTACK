1a42ea36f50fac33fedbceb2ed44a9af42a3835e
FDASEEM <jr.tecnon@gmail.com>
2025-05-14 00:36:40 -0400

    cambios minimos

--- hospital_vm/src/main/java/com/hospital_vm/hospital_vm/Controller/PacienteController.java
+++ hospital_vm/src/main/java/com/hospital_vm/hospital_vm/Controller/PacienteController.java
@@ -2,7 +2,7 @@ package com.hospital_vm.hospital_vm.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -82,6 +82,7 @@ public ResponseEntity<List<Paciente>> listar() {
     } catch (Exception e) {
         return ResponseEntity.notFound().build();
     }
+    
  }
 }
 
