500c6ae8fb2b3adeca7474c8e000a96f8c10146d
Your Name <you@example.com>
2025-05-13 21:37:27 -0400

    joincolum listo

--- hospital_vm/src/main/java/com/hospital_vm/hospital_vm/Model/Atencion.java
+++ hospital_vm/src/main/java/com/hospital_vm/hospital_vm/Model/Atencion.java
@@ -11,16 +11,19 @@ import java.util.Date;
 @NoArgsConstructor
 public class Atencion {
 
-
+    @ManyToOne
+    @JoinColumn(name = "medico_rut")
     @Column(nullable = false)
     private Medico medico;
 
     @Column(nullable = false)
     private String diagnostico;
 
+    @ManyToOne
+    @JoinColumn(name = "paciente_id")
     @Column(nullable = false)
     private Paciente paciente;
-
+    
     @Column(nullable = false)
     private Date fechaAtencion;
 
